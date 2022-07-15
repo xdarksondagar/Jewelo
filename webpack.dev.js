8aeaf5449dad8c9a551ca4914943cec75136ff9b
Jeet <jeetpatel0070070@gmail.com>
2022-07-15 21:42:28 +0530

    setup whole webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,59 +1,92 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const dirApp = path.resolve(__dirname, "src/js");
+const dirStyles = path.resolve(__dirname, "src/scss");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
   entry: {
-    index: "./src/js/index.js"
+    app: path.resolve(dirApp, "index.js"),
+    styles: path.resolve(dirStyles, "index.scss")
   },
   devtool: "inline-source-map",
   devServer: {
-    static: "./dist",
+    static: "./dist"
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       inject: true,
-      chunks: ["index"],
-      filename: "index.html",
+      chunks: ["app", "styles"],
+      filename: "index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
     })
   ],
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
+    clean: true
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: ""
+            }
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: false
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: false
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: false
+            }
+          }
+        ]
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: "asset/resource"
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
+        type: "asset/resource"
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader",
         options: {
-          presets: ["@babel/preset-env"],
-        },
+          presets: ["@babel/preset-env"]
+        }
       },
       {
         test: /\.html$/i,
-        loader: "html-loader",
+        loader: "html-loader"
       },
       {
         test: /\.(glsl|vs|fs|vert|frag)$/,
         exclude: /node_modules/,
-        use: ["raw-loader", "glslify-loader"],
-      },
-    ],
-  },
+        use: ["raw-loader", "glslify-loader"]
+      }
+    ]
+  }
 };
